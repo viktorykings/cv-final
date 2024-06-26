bcd90292c04e94c1daac0f32cda590067c9437ab
viktoria <kingsviktory@gmail.com>
2024-06-26 17:21:27 +0300

    refactor: move create client login into separate file

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,5 +1,5 @@
 import { useLazyQuery } from '@apollo/client'
-import { LOGIN } from '../queries/auth'
+import { LOGIN } from '../graphql/queries/auth'
 import { LoginResult, LoginArgs } from '../types/queryTypes'
 
 export const useLogin = () => {
--- src/main.tsx
+++ src/main.tsx
@@ -1,53 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink, from } from '@apollo/client'
-import { setContext } from '@apollo/client/link/context'
+import { ApolloProvider } from '@apollo/client'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from '@emotion/react'
-import theme from './styles/theme.tsx'
 import { CssBaseline } from '@mui/material'
-import { AUTH_TOKEN } from './constants/constants.ts'
-import { onError } from '@apollo/client/link/error'
-import showToast from './utils/showToast.tsx'
+import { client } from './graphql/auth/client.ts'
+import theme from './styles/theme.tsx'
 import './index.css'
-
-const httpLink = createHttpLink({
-  uri: import.meta.env.VITE_HTTP_LINK
-})
-
-const errorLink = onError(({ graphQLErrors, networkError }) => {
-  if (graphQLErrors) {
-    console.log(graphQLErrors)
-    graphQLErrors.forEach(({ message }) => {
-      if (message === 'Invalid credentials') {
-        showToast('Wrong email or password')
-      } else if (message.match('duplicate key value')) {
-        showToast('User already exist')
-      } else {
-        showToast(message)
-      }
-    })
-    if (networkError) showToast(networkError.toString())
-  }
-})
-
-const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
-  const token = localStorage.getItem(AUTH_TOKEN)
-  // return the headers to be used in the HTTP request
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : ''
-    }
-  }
-})
-
-export const client = new ApolloClient({
-  link: from([authLink, errorLink, httpLink]),
-  cache: new InMemoryCache()
-})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
