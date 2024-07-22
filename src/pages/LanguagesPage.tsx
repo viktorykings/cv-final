0198c4c0c85b3e1988c11a12089142cfcb8cb202
viktoria <kingsviktory@gmail.com>
2024-07-22 23:38:18 +0300

    feat: show list of languages

--- src/pages/LanguagesPage.tsx
+++ src/pages/LanguagesPage.tsx
@@ -1,4 +1,27 @@
+import { useState } from 'react'
+import SearchBar from '../shared/components/Search'
+import Table from '../shared/components/Table'
+import { useGetLanguages } from '../graphql/languages/hooks/useGetAllLanguages'
+
 const LanguagesPage = () => {
-  return <div>langs</div>
+  const { data: languages } = useGetLanguages()
+  const [searchQuery, setSearchQuery] = useState('')
+  if (!languages) return <>no languages</>
+  return (
+    <>
+      <SearchBar setSearchQuery={setSearchQuery} />
+
+      <Table
+        data={languages.languages.map(({ name, native_name, iso2, id }) => ({
+          name,
+          native_name,
+          iso2,
+          id
+        }))}
+        constextMenu={[]}
+        searchQuery={searchQuery}
+      />
+    </>
+  )
 }
 export default LanguagesPage
