5d46a7239817a7e00248198180da4da18766f9cc
viktoria <kingsviktory@gmail.com>
2024-07-23 23:33:47 +0300

    feat: switch language handler

--- src/shared/utils/locales/i18n.ts
+++ src/shared/utils/locales/i18n.ts
@@ -2,10 +2,13 @@ import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
 import en from '../../../../public/locales/en/translation.json'
 import ru from '../../../../public/locales/ru/translation.json'
-
+// import HttpBackend from 'i18next-http-backend';
+// import LanguageDetector from 'i18next-browser-languagedetector';
 export const defaultNS = 'translation'
 
 i18n
+  // .use(HttpBackend)
+  // .use(LanguageDetector)
   .use(initReactI18next) // passes i18n down to react-i18next
   .init({
     fallbackLng: 'en',
@@ -17,6 +20,7 @@ i18n
         translation: ru
       }
     },
+    debug: true,
     lng: 'ru', // language to use, more information here: https://www.i18next.com/overview/configuration-options#languages-namespaces-resources
     // you can use the i18n.changeLanguage function to change the language manually: https://www.i18next.com/overview/api#changelanguage
     // if you're using a language detector, do not define the lng option
